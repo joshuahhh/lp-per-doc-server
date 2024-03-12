8700e22c256437285b3a214679fc80c65c5ec150
Joshua Horowitz <joshuah@alum.mit.edu>
2024-03-12 13:05:05 -0700

    commit

--- docker-push.sh
+++ docker-push.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -e
+set -o xtrace
 
 VERSION=$(git log -1 --pretty=%h)
 
-docker push joshuahhh/lp-per-doc-server-amd64:$VERSION
 docker tag joshuahhh/lp-per-doc-server-amd64:$VERSION joshuahhh/lp-per-doc-server-amd64:latest
-docker push joshuahhh/lp-per-doc-server-arm64:$VERSION
+docker push joshuahhh/lp-per-doc-server-amd64:$VERSION
+docker push joshuahhh/lp-per-doc-server-amd64:latest
 docker tag joshuahhh/lp-per-doc-server-arm64:$VERSION joshuahhh/lp-per-doc-server-arm64:latest
+docker push joshuahhh/lp-per-doc-server-arm64:$VERSION
+docker push joshuahhh/lp-per-doc-server-arm64:latest
 
 # giving up on multi-arch for now
 
